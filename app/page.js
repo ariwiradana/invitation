f0d730ff6292577bb821ab80e057fddc12f665f4
Ari Wiradana <ariwiradana5@gmail.com>
2023-09-06 18:58:23 +0800

    asda

--- app/page.js
+++ app/page.js
@@ -1,17 +1,22 @@
 "use client";
-import PlayButton from "@/components/elements/play.button/app";
 import Cover from "@/components/partials/cover/app";
-import Persons from "@/components/partials/persons/app";
 import Wishes from "@/components/partials/wishes/app";
+import Persons from "@/components/partials/persons/app";
 import useAOS from "@/hooks/useAOS";
+import Ceremony from "@/components/partials/ceremony/app";
+import PlayButton from "@/components/elements/play.button/app";
+import usePlayMusic from "@/hooks/usePlayMusic";
 
 const Home = () => {
   useAOS();
+  const { togglePlay, play, audioRef } = usePlayMusic();
   return (
     <>
-      <Cover name="Nama Undangan" />
+      <PlayButton audioRef={audioRef} play={play} togglePlay={togglePlay} />
+      <Cover togglePlay={togglePlay} name={"Nama Undangan"} />
       <Persons />
-      {/* <Wishes /> */}
+      <Ceremony />
+      <Wishes />
     </>
   );
 };
